4d8626a7044f08894cbafa89fecd8a4f30fd1337
unknown <202080505@psu.palawan.edu.ph>
2022-09-12 01:33:34 +0800

    inc directory done

--- contacts pages.php
+++ contacts pages.php
@@ -1,28 +1,28 @@
 <?php include 'inc/header.php'; ?>
 
-<title> Home Page </title>
+<title> Contact Page </title>
 
 <header class="masthead mb-auto">
         <div class="inner">
-          <h3 class="masthead-brand">Home</h3>
+          <h3 class="masthead-brand">Contacts</h3>
           <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="index.php">Home</a>
+            <a class="nav-link " href="index.php">Home</a>
             <a class="nav-link" href="features.php">Features</a>
-            <a class="nav-link" href="contacts pages.php">Contact</a>
+            <a class="nav-link active" href="contacts pages.php">Contact</a>
           </nav>
         </div>
       </header>
 
 
       <main role="main" class="inner cover">
-        <h1 class="cover-heading">Home page.</h1>
-        <p class="lead">A home page is the default or front page of a site. It is the first page that visitors see when they load a URL. </p>
+        <h1 class="cover-heading">This is Contacts Pages.</h1>
+        <p class="lead">A contact page is a common web page on a website for visitors to contact the organization or individual providing the website. </p>
         <p class="lead">
           <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
         </p>
       </main>
 
-      
+     
 
  <?php include 'inc/footer.php'; ?>
    
--- features.php
+++ features.php
@@ -1,28 +1,26 @@
 <?php include 'inc/header.php'; ?>
 
-<title> Home Page </title>
+<title> Feature Page </title>
 
 <header class="masthead mb-auto">
         <div class="inner">
-          <h3 class="masthead-brand">Home</h3>
+          <h3 class="masthead-brand">Feature</h3>
           <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="index.php">Home</a>
-            <a class="nav-link" href="features.php">Features</a>
+            <a class="nav-link " href="index.php">Home</a>
+            <a class="nav-link active" href="features.php">Features</a>
             <a class="nav-link" href="contacts pages.php">Contact</a>
           </nav>
         </div>
       </header>
 
-
       <main role="main" class="inner cover">
-        <h1 class="cover-heading">Home page.</h1>
-        <p class="lead">A home page is the default or front page of a site. It is the first page that visitors see when they load a URL. </p>
+        <h1 class="cover-heading">This is features.</h1>
+        <p class="lead">Feature pages act as a landing page or a specialized collection of your Medium publication's contents. </p>
         <p class="lead">
           <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
         </p>
       </main>
 
-      
-
+     
  <?php include 'inc/footer.php'; ?>
    
